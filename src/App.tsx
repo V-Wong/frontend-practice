cdd294fd8e989fee272bd628b7f260d6f6ab4ca9
Vincent Wong <vincent@vwong.dev>
2025-06-01 19:23:07 +1000

    feat(progress-bar): Add smooth Progress Bar implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 
 import Home from "./pages/Home";
+import Progress from "./pages/Progress";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/progress-bar" element={<Progress />} />
       </Routes>
     </BrowserRouter>
   );
