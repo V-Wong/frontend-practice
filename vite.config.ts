d0e70c66113d5486df42751ef564fb879370b9b1
Vincent Wong <vincent@vwong.dev>
2025-06-02 22:20:00 +1000

    ci(gh-pages): Add workflow to deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
+import { githubPagesSpa } from "@sctg/vite-plugin-github-pages-spa";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: "frontend-practice",
+  plugins: [githubPagesSpa({ verbose: true }), react(), tailwindcss()],
 });
