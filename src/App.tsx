7f8c61cf76993aea2d4ba2613343a3939a5371a0
Vincent Wong <vincent@vwong.dev>
2025-06-09 21:43:51 +1000

    refactor(router): Extract routes into mappable array

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,27 @@ import Home from "./pages/Home";
 import ImageLoader from "./pages/ImageLoader";
 import Progress from "./pages/Progress";
 import TicTacToe from "./pages/TicTacToe";
+import type { JSX } from "react";
+
+type PageToElement = {
+  path: string;
+  element: JSX.Element;
+};
+
+const PAGES: PageToElement[] = [
+  { path: "/progress-bar", element: <Progress /> },
+  { path: "/tic-tac-toe", element: <TicTacToe /> },
+  { path: "/image-loader", element: <ImageLoader /> },
+];
 
 function App() {
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/progress-bar" element={<Progress />} />
-        <Route path="/tic-tac-toe" element={<TicTacToe />} />
-        <Route path="/image-loader" element={<ImageLoader />} />
+        {PAGES.map(({ path, element }) => (
+          <Route path={path} element={element} />
+        ))}
       </Routes>
     </BrowserRouter>
   );
